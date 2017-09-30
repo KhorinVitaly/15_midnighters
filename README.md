6cbde4e142a07d85d970e52518ffc16c48d00938
Vitaly <vitaly@MacBook-Pro-Vitaly.local>
2017-10-01 00:24:44 +0600

    first commit, solve for task

--- README.md
+++ README.md
@@ -1,6 +1,29 @@
 # Night Owls Detector
 
-[TODO. There will be project description]
+This script for Devman's student. If you can look in terminal who of your colleagues work at night, you can do it with
+this script.
+
+
+#How to Install
+
+Python 3 should be already installed. Then use pip (or pip3 if there is a conflict with old Python 2 setup) to install
+dependencies:
+
+```
+pip install -r requirements.txt # alternatively try pip3
+```
+
+#Quickstart
+
+Just launch seek_dev_nighters.py in terminal 
+
+```
+Devman has midnighters:
+id197089895
+jewta94
+linuxoid69
+```
+Remember, it is recommended to use virtualenv/venv for better isolation.
 
 # Project Goals
 
--- seek_dev_nighters.py
+++ seek_dev_nighters.py
@@ -1,15 +1,55 @@
+import requests
+from pytz import timezone
+from datetime import datetime, time
+
+URL = 'https://devman.org/api/challenges/solution_attempts/'
+NIGHT_TIME = {'start': time(0, 0, 0, 0, None),
+              'end': time(6, 0, 0, 0, None)}
+
+
 def load_attempts():
-    pages = 1
-    for page in range(pages):
-        # FIXME подключить загрузку данных из API
-        yield {
-            'username': 'bob',
-            'timestamp': 0,
-            'timezone': 'Europe/Moscow',
-        }
+    content = fetch_content()
+    if content:
+        pages = content['number_of_pages']
+    else:
+        return []
+    for page in range(1, pages):
+        for record in fetch_content(page)['records']:
+            yield {
+                'username': record['username'],
+                'timestamp': record['timestamp'],
+                'timezone': record['timezone'],
+            }
+
 
 def get_midnighters():
-    pass
+    midnighters = []
+    for attempt in load_attempts():
+        if attempt['timestamp']:
+            attempt_date = datetime.fromtimestamp(float(attempt['timestamp']))
+            student_time = timezone(attempt['timezone']).fromutc(attempt_date).time()
+            if student_time >= NIGHT_TIME['start'] and student_time < NIGHT_TIME['end']:
+                attempt['time'] = str(student_time)
+                midnighters.append(attempt['username'])
+    return set(midnighters)
+
+
+def fetch_content(page=1):
+    try:
+        return requests.get(URL, params={'page': page}).json()
+    except requests.HTTPError as error:
+        print('HTTP Error!')
+        print('Response is: {0}'.format(error.response.content))
+    except requests.ConnectionError:
+        print('Connection failed!')
+    return None
+
 
 if __name__ == '__main__':
-  pass
+    midnighters = get_midnighters()
+    if len(midnighters):
+        print('Devman has midnighters:')
+        for username in midnighters:
+            print(username)
+    else:
+        print('Devman does not have midnighters')
